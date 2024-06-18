3b8d2ac04abf4502fd588b1b5d04995ad990d349
Christian Bacani <bacani.c@labrador-company.com>
2024-06-18 19:19:33 +0800

    added new line in cypress/e2e/add-client.cy.js

--- cypress/e2e/add-client.cy.js
+++ cypress/e2e/add-client.cy.js
@@ -20,14 +20,14 @@ it('User Page', function(){
     cy.visit('https://www.fr.digital-report.net:8443/en/admin/users/')
     // cy.wait(10000)
     cy.get('.text-lg-right > .btn').click()
-    cy.get('#user_add_lastname').type('Bacani')
-    cy.get('#user_add_firstname').type('Christian')
+    cy.get('#user_add_lastname').type('Khalifa')
+    cy.get('#user_add_firstname').type('Bones')
     cy.get('#user_add_email').type('test@labrador.com')
     cy.get('.col-12 > div > .select2 > .selection > .select2-selection').click()
     cy.get('#select2-user_add_customers-results').click()
-    cy.get('#user_add_customers').select(['3', '118'], {force:true} ) //we used {force:true} because the dropdown is getting close automatically once you a client is already selected 
+    cy.get('#user_add_customers').select(['3', '118'], {force:true} ) //we used {force:true} because the dropdown is getting close automatically once a client is already selected 
     cy.get('.switch-custom').click()
-    // cy.get('#select2-reports-container').click()
+    cy.wait(1000)
     cy.get('.btn-primary').click()
 
     
